08a0e49e64a5cf30140180e798b21d5fd7aba72e
a2025120564 <a2025120564@noreply.gitcode.com>
2026-01-10 22:20:01 +0800

    chore: update site and profile config

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/touxiang.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Jasper Zhang",
 	bio: "一个热爱生活渴望自由的人",
 	links: [
